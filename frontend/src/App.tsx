42ab94f396a950f942b800f3f97d4ff0752c7fea
miilanez <gabrielmilanez99@hotmail.com>
2024-11-08 23:36:11 -0300

    :wrench: feat: routing pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      Erro 404
+    </div>
+  )
+}
+
+export default NotFound
